1de630fb711d3f66645b26d62c874d221797243b
sannaga3 <n.tetsuya02@gmail.com>
2021-08-23 16:55:41 +0900

    ElementUIでTodoリストを装飾

--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -2,14 +2,16 @@
   <div>
     <h1>Todoリスト</h1>
     <form @submit.prevent="addTodo()">
-      <input v-model="todo">
+      <el-input placeholder="Please input todo" v-model="todo"></el-input>
     </form>
-    <ul>
-      <li v-for="( todo, index ) in todos" :key="index">
-        {{ todo }}
-        <button @click="removeTodo(index)">完了</button>
-      </li>
-    </ul>
+    <el-row :gutter="36">
+      <el-col :span="6"  v-for="( todo, index ) in todos" :key="index">
+        <el-card class="box-card bg-purple-light" shadow="hover" style="margin: 5px 0;">
+          {{ todo }}
+          <el-button type="danger" style="margin-left: 30px;" icon="el-icon-delete" circle @click="removeTodo(index)"></el-button>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -33,3 +35,32 @@ export default {
   }
 }
 </script>
+
+<style>
+  .el-row {
+    margin-bottom: 20px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+</style>
