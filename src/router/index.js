a016791577fb538094927893c6c14d3fa24771d4
sannaga3 <n.tetsuya02@gmail.com>
2021-08-23 15:46:23 +0900

    TodoListのルーティング設定。Todoの作成・表示・削除機能作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import TodoList from '../views/TodoList.vue'
+
 
 Vue.use(VueRouter)
 
@@ -13,9 +15,6 @@ const routes = [
   {
     path: '/todo-list',
     name: 'TodoList',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: TodoList
   }
 ]
--- src/views/TodoList.vue
+++ src/views/TodoList.vue
@@ -1,10 +1,35 @@
 <template>
   <div>
     <h1>Todoリスト</h1>
+    <form @submit.prevent="addTodo()">
+      <input v-model="todo">
+    </form>
+    <ul>
+      <li v-for="( todo, index ) in todos" :key="index">
+        {{ todo }}
+        <button @click="removeTodo(index)">完了</button>
+      </li>
+    </ul>
   </div>
 </template>
+
 <script>
 export default {
   name: 'TodoList',
+  data() {
+    return {
+      todo: '',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push(this.todo);
+      this.todo = '';
+    },
+    removeTodo(index){
+      this.todos.splice(index, 1); //--1
+    }
+  }
 }
 </script>
